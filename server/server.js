35a57820da6f6eefecc8d160042d36b7ff4722b6
mohamed sadik <sadiks>
2023-01-05 18:02:18 +0530

    service call

---
+++ server/server.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const API_KEY = '';
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+app.post('/chat', async (req, res) => {
+    const { prompt } = req.body;
+    console.log(`prompt ${prompt}`);
+    const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    temperature: 0.9,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.6,
+    });
+    console.log(response.data.choices[0].text);
+    res.send(response.data.choices[0].text);
+});
