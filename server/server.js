46234c5c7f8fb7e7d161b16fa2cf810df42c6a17
mohamed sadik <sadiks>
2023-01-05 18:22:37 +0530

    few changes on commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const API_KEY = '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';
+const API_KEY = '';
 app.listen(5000, () => {
   console.log('Server listening on port 5000');
 });
@@ -22,13 +22,13 @@ app.post('/chat', async (req, res) => {
     const { prompt } = req.body;
     console.log(`prompt ${prompt}`);
     const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: prompt,
-    temperature: 0.9,
-    max_tokens: 150,
-    top_p: 1,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.6,
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.6,
     });
     console.log(response.data.choices[0].text);
     res.send(response.data.choices[0].text);
